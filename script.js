5279fd7a5fb9584f9c65863c96c9b26807cda95a
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-05-27 01:44:56 +0300

    создала папку для практики js

---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log(5 && 7 && 0 && 'hello')
+
+// console.log(!false)
+
+// console.log( 5 || false || null)
+
+// let balance = 10000;
+// const payment = 2000;
+
+// console.log(
+//     `Общая стоимость заказа ${payment} кредитов. Проверяем доступное кол-во средств на счету`,
+// );
+
+// if (payment <= balance) {
+//     balance -= payment;
+//     // balance = balance - payment
+
+//     console.log('Все хорошо, снимаем деньги... Спасибо за покупку!');
+//     console.log(`На счету осталось ${balance} кредитов`);
+// } else {
+//     console.log('На счету недостаточно средств для продолжения операции!');
+// }
+
+// console.log('Операция завершена');
+
+// let totalSpent = 2000;
+// let payment = 500;
+// let discount = 0;
+
+// if (totalSpent >= 100 && totalSpent < 1000) {
+//     console.log(' Бронзовый партнер. Скидка 2%');
+//     discount = 0.02;
+// } else if (totalSpent >= 1000 && totalSpent < 5000) {
+//     console.log(' Серебрянный партнер. Скидка 5%');
+//     discount = 0.05;
+// } else if (totalSpent >= 5000) {
+//     console.log(' Золотой партнер. Скидка 10%');
+//     discount = 0.1; 
+// } else {
+//     console.log(' не партнер. Скидка 0%');  
+// }
+
+// payment -= payment * discount;
+// // payment = payment - payment * discount;
+
+// console.log(`Оформлен заказ на сумму ${payment} со скидкой ${discount * 100}%`);
+
+// totalSpent += payment;
+
+// console.log(`Общая сумма, потраченная в магазине: ${totalSpent}`);
