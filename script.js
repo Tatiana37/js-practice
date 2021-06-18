ea64012d33004e202800aa3a2757826c28f9e492
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-06-18 18:37:27 +0300

    добавила примеры задач из лекции Репеты 4го модуля 2я часть

--- script.js
+++ script.js
@@ -1437,3 +1437,28 @@
 // console.log(r3); // (2) [{...}, {...}]
 
 // ===========================================================
+
+"use strict";
+//  this
+//  при передаче метода объекта как колбэка 
+// контекст не сохраняется (но с bind мы получаем ссылку на объект)
+// const counter = {
+//     value: 0,
+//     increment(value) {
+//         console.log('increment -> this', this);
+//         this.value += value;
+//     },
+//     decrement(value) {
+//         console.log('decrement -> this', this);
+//         this.value -= value;
+        
+//     },
+// };
+
+// const updateCounter = function (value, operation) {
+//     operation(value);
+// };
+// updateCounter(10, counter.increment.bind(counter));
+// updateCounter(5, counter.decrement.bind(counter));
+
+// console.log(counter);
