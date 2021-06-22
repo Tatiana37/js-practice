56885b994d381b7591259920b7751e495910a517
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-06-22 18:19:21 +0300

    добавила примеры 5 модуля из лекции и конспекта

--- script.js
+++ script.js
@@ -1462,3 +1462,267 @@
 // updateCounter(5, counter.decrement.bind(counter));
 
 // console.log(counter);
+
+
+
+//  МОДУЛЬ 5
+
+// функция конструктор и свойство прототайп
+
+// const User = function ({ email, password } = {}) {
+//     this.email = email;
+//     this.password = password;
+// };
+
+// User.prototype.changeEmail = function (newEmail) {
+//     this.email = newEmail;
+// };
+
+// const mango = new User({ email: "mango@mail.com", password: 1111111 });
+
+// mango.changeEmail('my-new-mail@mail.com');
+// console.log(mango);
+
+//  1 у каждого объекта есть св-во __proto__
+//  2 в этом св-ве лежит ссылка на его ПРОТОТИП, те другой объект
+//  3 при написании кода в св-ве прото записывается  ...
+//  функция.prototype
+
+// const objA = {
+//     X: 5,
+// };
+
+// console.log(objA === Object.prototype); // false
+// console.log(objA.__proto__ === Object.prototype); // true
+
+//  4 ФУНКЦИЯ-конструктор - это просто функция!
+//  5 всю магию делает оператор NEW
+//  6 если ф-ция вызывается через New - создается пустой объект
+//  7 ф-ция вызывается в контексте созданного (пустого) объекта
+//  8 в свойстве __proto__  записывается ссылка на объект функция.prototype
+//  например : this = Object.create(User.prototype)
+//  9 ссылка на объект возвращается на место вызова new Функция()
+
+
+
+//  статические методы и свойства
+//  доступны только на самом конструкторе 
+//  в них нет this, или он бесполезен
+
+// const User = function ({ email, password } = {}) {
+//     this.email = email;
+//     this.password = password;
+// };
+
+// User.prototype.changeEmail = function (newEmail) {
+//     this.email = newEmail;
+// };
+
+// const mango = new User({ email: "mango@mail.com", password: 1111111 });
+
+// mango.changeEmail('my-new-mail@mail.com');
+// console.log(mango);
+
+// User.message = "я статическое св-во, меня нет на экземплярах";
+
+// User.logInfo = function (obj) {
+//     console.log('User.logInfo -> obj', obj);
+//     console.log('Почта: ', obj.email);
+//     console.log('Пароль: ', obj.password);
+// };
+
+// User.logInfo(mango);
+
+// Object.keys();
+// Object.values();
+
+// 1. Конструкторы
+
+// const Hotel = function (name, stars, capacity) {
+//   this.name = name;
+//   this.stars = stars;
+//   this.capacity = capacity;
+// };
+
+// const hotel = new Hotel('Resort Hotel', 5, 100);
+// console.log(hotel);
+// // Hotel {name: "Resort Hotel", stars: 5, capacity: 100}
+
+// const motel = new Hotel('Sunlight Motel', 4, 300);
+// console.log(motel);
+// // Hotel {name: "Sunlight Motel", stars: 4, capacity: 300}
+
+
+// 3. Добавление методов
+
+// const Hotel = function (name, stars, capacity) {
+//   this.name = name;
+//   this.stars = stars;
+//   this.capacity = capacity;
+//   this.guestCount = 0;
+
+//   this.greet = function (guestName) {
+//     console.log(`Hello ${guestName}, wellcome to ${this.name}`);
+//   };
+
+//   this.addGuests = function (amount) {
+//     this.guestCount += amount;
+//   };
+
+//   this.removeGuests = function (amount) {
+//     this.guestCount -= amount;
+//   };
+// };
+
+// const hotel = new Hotel('Sunrise Hotel', 5, 100);
+
+// console.log(hotel);
+// // Hotel {name: "Sunrise Hotel", greet: ƒ, addGuests: f, removeGuests: f}
+// hotel.greet('Mango'); // Hello Mango, wellcome to Sunrise Hotel
+// hotel.addGuests(50);
+// hotel.removeGuests(50);
+
+// Управляем магазином
+
+// (обычный метод - хард-код)
+// const mango = {
+//   name: 'Mango',
+//   sales: 5,
+//   sell(product) {
+//     this.sales += 1;
+//     return `Manager ${this.name} sold ${product}.`;
+//   },
+// };
+
+// console.log(mango.sales); // 5
+// console.log(mango.sell('TV')); // Manager Mango sold TV
+// console.log(mango.sell('Microwave')); // Manager Mango sold Microwave
+// console.log(mango.sales); // 7
+
+// const poly = {
+//   name: 'Poly',
+//   sales: 10,
+//   sell(product) {
+//     this.sales += 1;
+//     return `Manager ${this.name} sold ${product}.`;
+//   },
+// };
+
+// console.log(poly.sales); // 10
+// console.log(poly.sell('TV')); // Manager Poly sold TV
+// console.log(poly.sell('Microwave')); // Manager Poly sold Microwave
+// console.log(poly.sales); // 12
+
+//  (с помощью конструктора)
+
+// const Manager = function (name = 'manager', sales = 0) {
+//   this.name = name;
+//   this.sales = sales;
+
+//   this.sell = function (product) {
+//     this.sales += 1;
+//     return `Manager ${this.name} sold ${product}.`;
+//   };
+// };
+
+// const mango = new Manager('Mango', 5);
+// console.log(mango.sales); // 5
+// console.log(mango.sell('TV')); // Manager Mango sold TV
+// console.log(mango.sell('Microwave')); // Manager Mango sold Microwave
+// console.log(mango.sales); // 7
+
+// const poly = new Manager('Poly', 10);
+// console.log(poly.sales); // 10
+// console.log(poly.sell('TV')); // Manager Poly sold TV
+// console.log(poly.sell('Microwave')); // Manager Poly sold Microwave
+// console.log(poly.sales); // 12
+
+
+// Object.prototype.hasOwnProperty()
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// for (const key in dog) {
+//     console.log(key); // barks, eats
+    
+//     if (!dog.hasOwnProperty(key)) continue;
+
+//      console.log(key); // barks
+// }
+
+// Метод Object.keys(obj) вернет массив только собственных ключей объекта obj, поэтому рекомендуется использовать именно его.
+
+// const animal = { eats: true };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// const dogKeys = Object.keys(dog);
+
+// console.log(dogKeys); // ['barks']
+
+// Наследование и конструкторы
+// const Hero = function (name, xp) {
+//   this.name = name;
+//   this.xp = xp;
+// };
+
+// /*
+//  * Теперь у нас есть конструктор базового класса героя,
+//  * добавим в prototype какой-то метод.
+//  */
+// Hero.prototype.gainXp = function (amount) {
+//   console.log(`${this.name} gained ${amount} experience points`);
+//   this.xp += amount;
+// };
+
+// const mango = new Hero('Mango', 1000);
+// console.log(mango); // Hero { name: 'Mango', xp: 1000 }
+
+// // Так как mango это экземпляр Hero, то ему доступны методы из Hero.prototype
+// mango.gainXp(500); // Mango gained 500 experience points
+// console.log(mango); // Hero { name: 'Mango', xp: 1500 }
+
+// const Warrior = function (name, xp, weapon) {
+//   /*
+//    * Во время выполнения функции Warrior вызываем функцию Hero
+//    * в контексте объекта создающегося в Warrior, а так же передаем
+//    * аргументы для инициализации полей this.name и this.xp
+//    *
+//    * this это будущий экземпляр Warrior
+//    */
+//   Hero.call(this, name, xp);
+
+//   // Тут добавляем новое свойство - оружие
+//   this.weapon = weapon;
+// };
+
+// // Сразу добавим метод для атаки в prototype воина
+// Warrior.prototype.attack = function () {
+//   console.log(`${this.name} attacks with ${this.weapon}`);
+// };
+
+// const poly = new Warrior('Poly', 200, 'sword');
+
+// console.log(poly); // Warrior {name: "Poly", xp: 200, weapon: "sword"}
+// poly.attack(); // Poly attacks with sword
+// // poly.gainXp(); // Uncaught TypeError: poly.gainXp is not a function
+
+// /*
+//  * Используем Object.create() для того чтобы изначально записать
+//  * в Warrior.prototype пустой объект, в __proto__ которого будет
+//  * ссылка на Hero.prototype. Это необходимо сделать до того
+//  * как добавлять методы
+//  */
+// Warrior.prototype = Object.create(Hero.prototype);
+
+// // Не забываем добавить в Warrior.prototype свойство constructor
+// Warrior.prototype.constructor = Warrior;
+
+// // Добавим метод для атаки
+// Warrior.prototype.attack = function () {
+//   console.log(`${this.name} attacks with ${this.weapon}`);
+// };
+
+// // Попробуем теперь
+// poly.gainXp(300); // Poly gained 300 experience points
